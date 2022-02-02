e473e8ffc420d2ac394cf0971d04af2fa37d6b6a
Elizabeth <elizabeth.por@gmail.com>
2022-02-01 18:37:01 -0600

    commit tree e0fe894d5f5a7e12bf3a1aee198d078c0d9074d8

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Code your solution in this file!
+// function distanceFromHqInBlocks(startingPoint) {
+//     if (startingPoint > 42) {
+//     return startingPoint - 42;
+//     } else (startingPoint <= 42)
+//         return 42 - startingPoint;
+// }
+
+  function distanceFromHqInBlocks(startingPoint) {
+      return Math.abs(startingPoint - 42);
+  }
+
+function distanceFromHqInFeet(startingPoint) {
+    return (distanceFromHqInBlocks(startingPoint)) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+
+// function calculatesFarePrice(start, destination) {
+//     let distance = distanceFromHqInFeet(start, destination);
+//     let price = 0;
+//     switch (true) {
+//         case distance < 400:
+//             price = 0;
+//             break;
+//         case 400 < distance < 2000:
+//             price = (distance - 400) * 0.02;
+//             break;
+//         case 2000 < distance <= 2500:
+//             price = 25;
+//             break;
+//         case distance > 2500:
+//             return "cannot travel that far";
+//     }
+//     return price;
+// }
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+//    let price = 0;
+    switch (true) {
+        case distance > 2500:
+            return "cannot travel that far"
+        case distance > 2000:
+            return 25;
+        case distance > 400:
+            return (distance - 400) * 0.02;
+        default :
+            return 0;
+    }
+}
